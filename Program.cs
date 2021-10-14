5991ac979eb3c7c406677112e8e93cb6e1d89287
Anusha98s <92158408+Anusha98s@users.noreply.github.com>
2021-10-14 10:26:22 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Looping_Age
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter yur age");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if(age>18)
+            {
+                Console.WriteLine("Eligible for voting");
+            }
+            else
+            {
+                Console.WriteLine("Not Eligible for voting");
+            }
+        }
+    }
+}
